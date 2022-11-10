0a66312d783c94b891cbf99efe3e62f035621356
Mayque <mayque.731@gmail.com>
2022-11-09 20:35:16 -0400

    dinamicidade do site concertada, conteudo atualizado

--- routes/base.js
+++ routes/base.js
@@ -19,4 +19,15 @@ router.get('/perfil', function (request, response) {
     response.render('perfil');
 });
 
+router.get('/pesquisar', function (request, response) {
+    response.render('pesquisar');
+});
+
+router.get('/notificacoes', function (request, response) {
+    response.render('notificacoes');
+});
+
+router.get('/configuracoes', function (request, response) {
+    response.render('configuracoes');
+});
 module.exports = router;
